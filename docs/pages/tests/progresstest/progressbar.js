209d2df63005a8c6c43a54453b0e96d909d18e7d
yogsoy <yogsoy@users.noreply.github.com>
2018-05-08 13:52:40 +1000

    did some stuff

--- docs/pages/tests/progresstest/progressbar.js
+++ docs/pages/tests/progresstest/progressbar.js
@@ -1,15 +1,16 @@
 var p = 0;
 var s = 0
+var csv = "";
 function getNewValue() {
-  p = p + 1/*Math.random() * 1000*/;
+  p = Math.random() * 1000;
   document.getElementById("progress").value = p;
+  console.log(p);
+  csv += p + ",";
 }
 function resetValue() {
   p = 0;
   document.getElementById("progress").value = p;
 }
-while (s < 10000) {
-  s++;
-  p = p + 1/*Math.random() * 1000*/;
-  document.getElementById("progress").value = p;
+function getCSV() {
+  console.log(csv);
 }
